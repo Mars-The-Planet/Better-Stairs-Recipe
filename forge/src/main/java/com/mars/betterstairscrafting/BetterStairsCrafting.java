e285e088c9a2fa495a82bf8703fab5a5476c00a8
Matej <ruzicka.maty@gmail.com>
2025-06-05 16:18:15 +0200

    ported to 1.20.1

---
+++ forge/src/main/java/com/mars/betterstairscrafting/BetterStairsCrafting.java
@@ -0,0 +1,10 @@
+package com.mars.betterstairscrafting;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class BetterStairsCrafting {
+    public BetterStairsCrafting() {
+        CommonClass.init();
+    }
+}
